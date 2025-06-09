b0ea8ab6fd6ff1d1ab0a9eca244d3c3fa6ce6871
Guilherme Chafy <guichafy@gmail.com>
2025-06-09 01:59:26 -0300

    refactor: atualiza teste de integração do TodoAdapter para usar GenericContainer e configurações programáticas do WireMock

--- src/test/java/guichafy/sample_api/todo/TodoAdapterIntegrationTest.java
+++ src/test/java/guichafy/sample_api/todo/TodoAdapterIntegrationTest.java
@@ -5,10 +5,14 @@ import guichafy.sample_api.domain.valueobjects.TodoId;
 import guichafy.sample_api.domain.entities.Todo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.wiremock.integrations.testcontainers.WireMockContainer;
+import org.testcontainers.utility.DockerImageName;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,16 +23,78 @@ import static org.junit.jupiter.api.Assertions.*;
 class TodoAdapterIntegrationTest {
 
     @Container
-    static WireMockContainer wiremock = new WireMockContainer("wiremock/wiremock:3.3.1")
-            .withMappingFromResource("todos.json")
-            .withMappingFromResource("todo-1.json");
+    static GenericContainer<?> wiremock = new GenericContainer<>(DockerImageName.parse("wiremock/wiremock:3.9.1"))
+            .withExposedPorts(8080)
+            .withCommand("--disable-banner");
 
     private TodoAdapter todoAdapter;
+    private String wiremockUrl;
 
     @BeforeEach
     void setup() {
+        wiremockUrl = String.format("http://%s:%d", 
+            wiremock.getHost(), 
+            wiremock.getMappedPort(8080));
+        
         RestTemplate restTemplate = new RestTemplate();
-        todoAdapter = new TodoAdapter(restTemplate, wiremock.getBaseUrl());
+        todoAdapter = new TodoAdapter(restTemplate, wiremockUrl);
+        
+        // Setup WireMock mappings programmatically
+        setupWireMockStubs();
+    }
+    
+    private void setupWireMockStubs() {
+        TestRestTemplate testRestTemplate = new TestRestTemplate();
+        
+        // Setup stub for GET /todos
+        String todosStub = """
+            {
+              "request": {
+                "method": "GET",
+                "url": "/todos"
+              },
+              "response": {
+                "status": 200,
+                "headers": {"Content-Type": "application/json"},
+                "jsonBody": [
+                  {"userId": 1, "id": 1, "title": "delectus aut autem", "completed": false},
+                  {"userId": 1, "id": 2, "title": "quis ut nam facilis et officia qui", "completed": false}
+                ]
+              }
+            }
+            """;
+            
+        testRestTemplate.postForEntity(
+            wiremockUrl + "/__admin/mappings",
+            todosStub,
+            String.class
+        );
+        
+        // Setup stub for GET /todos/1
+        String todoStub = """
+            {
+              "request": {
+                "method": "GET",
+                "url": "/todos/1"
+              },
+              "response": {
+                "status": 200,
+                "headers": {"Content-Type": "application/json"},
+                "jsonBody": {
+                  "userId": 1,
+                  "id": 1,
+                  "title": "delectus aut autem",
+                  "completed": false
+                }
+              }
+            }
+            """;
+            
+        testRestTemplate.postForEntity(
+            wiremockUrl + "/__admin/mappings",
+            todoStub,
+            String.class
+        );
     }
 
     @Test
